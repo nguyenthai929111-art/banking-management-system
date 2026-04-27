e01390e3f713325619090838bd24198c5a05b1ab
nguyenthai929111-art <nguyenthai929111@gmail.com>
2026-04-27 23:32:07 +0700

    Update bank_wrapper.cpp

--- bank_wrapper.cpp
+++ bank_wrapper.cpp
@@ -6,12 +6,12 @@ namespace py = pybind11;
 PYBIND11_MODULE(bank_core, m) {
     py::class_<BankManager>(m, "BankManager")
         .def(py::init<>())
-        .def("get_history", &BankManager::get_history)
         .def("create_account", &BankManager::create_account)
         .def("authenticate", &BankManager::authenticate)
         .def("get_balance", &BankManager::get_balance)
         .def("deposit", &BankManager::deposit)
         .def("withdraw", &BankManager::withdraw)
         .def("transfer", &BankManager::transfer)
-        .def("request_loan", &BankManager::request_loan);
+        .def("request_loan", &BankManager::request_loan)
+        .def("get_history", &BankManager::get_history);
 }
