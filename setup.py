ab32b168ded11e281317b73f92f1ba7c13ff89f3
nguyenthai929111-art <nguyenthai929111@gmail.com>
2026-04-28 23:52:15 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,8 +17,7 @@ functions_module = Extension(
     include_dirs=[get_pybind_include(), '.'],
     libraries=libs,
     language='c++',
-    extra_compile_args=['-std=c++11'],
-    extra_link_args=link_args
+    extra_compile_args=['-std=c++11']
 )
 
 setup(
